fb38c18dd4aac9b6c0b1c6f8f313fe4e2516e218
xleonard1 <xavier.lnrd@gmail.com>
2022-08-27 20:59:14 -0400

    added form state to jumnboTron

--- client/src/components/Jumbotron/index.js
+++ client/src/components/Jumbotron/index.js
@@ -1,11 +1,53 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Button from 'react-bootstrap/Button';
+
+
+
+const styles = {
+    getInfo: {
+        display: 'inline-flex',
+        justifyContent: 'space-between'
+
+    },
+    input: {
+        borderRadius: '10px',
+        padding: '10px',
+        marginRight: '10px'
+    },
+    button: {
+        padding: '10px',
+    },
+}
 
 export default function JumboTron() {
+    const [formState, setFormState] = useState({ email: '' });
+
+    const handleChange = (event) => {
+        const { name, value } = event.target;
+        setFormState({
+            ...formState,
+            [name]: value,
+        })
+    }
    return (
        <div
-       style ={{height: 560, clear: 'both', paddingTop: 120, textAlign: 'center', backgroundColor: 'black' }}
+       style ={{height: 560, clear: 'both', paddingTop: 120, textAlign: 'center'}}
        >
-           <h1>Attorney Case Management</h1>
+           <h1>Legal Case Management for Unparalleled Productivity</h1>
+           <p> The Easiest Way to Manage Your Large Client Case Load </p>
+        <div className = 'getmoreInfo' style={styles.getInfo} >  
+           <input
+              style={styles.input}
+              placeholder="youremail@test.com"
+              name="email"
+              type="email"
+              id="email"
+              onChange={handleChange}
+           />
+           <div className="flex-row flex-end">
+             <Button variant="primary" style={styles.button}>Get More Information</Button>{' '}
+           </div>
+        </div> 
        </div>
    );
 }
