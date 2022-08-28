6e4517c39e14d4436f6b12ec2a6b5cf68a3ff79e
xleonard1 <xavier.lnrd@gmail.com>
2022-08-27 20:15:22 -0400

    rendered react-bootstrap navbar

---
+++ client/src/pages/Homes.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import JumboTron from '../components/JumnboTron'
+
+
+const Home = () => {
+  return (
+      <div className="container">
+        
+
+      
+      </div>
+  )
+}
