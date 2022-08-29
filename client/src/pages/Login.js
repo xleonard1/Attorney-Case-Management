36c9692d54756163fa50e62f8ce9884d714a2ff5
xleonard1 <xavier.lnrd@gmail.com>
2022-08-29 17:20:39 -0400

    added form styling for login. adding form handling next

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,13 +1,28 @@
 import React, { useState } from 'react'
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button'
+import Alert from 'react-bootstrap/Alert'
+
+
+const styles = {
+    formStyle: {
+        maxWidth: '100%',
+        marginLeft: '40%',
+        marginTop: '20%',
+        marginBottom: '20%'
+    },
+    inputStyle: {
+        width: '300px'
+    }
+}
+
 
 
 
 export default function Login () {
     return (
-        <Form>
-          <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form style={styles.formStyle}>
+          <Form.Group className="mb-3" controlId="formBasicEmail" style={styles.inputStyle}>
             <Form.Label>Email address</Form.Label>
             <Form.Control type="email" placeholder="Enter email" />
             <Form.Text className="text-muted">
@@ -15,16 +30,18 @@ export default function Login () {
             </Form.Text>
           </Form.Group>
     
-          <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Group className="mb-3" controlId="formBasicPassword" style={styles.inputStyle}>
             <Form.Label>Password</Form.Label>
             <Form.Control type="password" placeholder="Password" />
           </Form.Group>
-          <Form.Group className="mb-3" controlId="formBasicCheckbox">
-            <Form.Check type="checkbox" label="Check me out" />
-          </Form.Group>
-          <Button variant="primary" type="submit">
+          <Button variant="primary" type="submit" style={styles.inputStyle}>
             Submit
           </Button>
+          <Form.Group>
+          <Form.Text className="text-muted">
+              Not a member? <Alert.Link href="/signup"> Sign up.</Alert.Link>
+          </Form.Text>
+          </Form.Group>
         </Form>
       );
 
